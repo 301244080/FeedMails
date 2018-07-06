d2eae796e23467931a6207864b072404df4ac0d3
Andrew <yy9577@gmail.com>
2018-07-05 22:19:32 -0700

    finished authentication workflow, setted up seperate keys for prod and dev

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// keys.js - figure out what set of credentials to return
+
+if (process.env.NODE_DEV === 'production') {
+    // we are in production - return the production set of keys
+    module.exports = require('./prod');
+} else {
+    // we are in development - return the dev set of keys
+    module.exports = require('./dev');
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,14 @@ module.exports = app => {
     );
 
     app.get('/auth/google/callback', passport.authenticate('google'));
+
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+
+    app.get('/api/current_user', (req, res) => {
+        // just to test if the user is correctly added to req.user
+        res.send(req.user);
+    });
 };
