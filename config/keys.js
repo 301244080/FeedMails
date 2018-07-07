6f5c65d5c66e257ffa330e91a4b254bff8761b7f
Andrew <yy9577@gmail.com>
2018-07-06 19:31:15 -0700

    fixed a typo in heroku enviroment variable

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // keys.js - figure out what set of credentials to return
 
-if (process.env.NODE_DEV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // we are in production - return the production set of keys
     module.exports = require('./prod');
 } else {
